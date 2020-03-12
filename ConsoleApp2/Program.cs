6964131d942573d444fe396321af19060cd3763f
윤어 <chyoon1985@gmail.com>
2020-03-12 20:16:52 +0900

    변경내용 테스트

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+
+    delegate void FeedBack(int value);
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // StaticDelegateDemo();
+
+
+            //var query = from x in Enumerable.Range(0, 100).Select(x => x * x);
+
+
+        }
+
+        private static void StaticDelegateDemo()
+        {
+            Console.WriteLine("----StaticDelegateDemo----");
+            Counter(1, 3, null);
+            Counter(1, 3, FeedbackToConsole);
+
+           // Action<int> n;
+
+            //n = Program.FeedbackToConsole;
+        }
+
+        private static void InstanceDelegateDemo()
+        {
+
+        }
+
+        private static void ChainDelegateDemo1(Program p)
+        {
+
+        }
+
+        private static void ChainDelegateDemo2(Program p)
+        {
+
+        }
+
+        private static void Counter(Int32 from, Int32 to, Action<int> fb)
+        {
+            for (int i = from; i <= to ; i++)
+            {
+                if (fb != null)
+                    fb(i) ;
+            }
+        }
+
+        private static void FeedbackToConsole(int value)
+        {
+            Console.WriteLine("FeedbackToConsole:" + value);
+        }
+
+        private static void FeedbackToMsgBox(int value)
+        {
+            Console.WriteLine("FeedbackToMsgBox:" + value);
+        }
+
+
+
+
+    }
+
+    
+}
